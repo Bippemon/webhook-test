1186d37af8e3ecbb6bdaa22774ee36ef8c9453ef
Simon <simonbrenning@hotmail.com>
2026-03-18 15:41:44 +0100

    big upgrads

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, timedelta
 from os.path import exists
-import json
+import os
 
 LOG_PATH = "logs/logs.csv"
 
@@ -10,7 +10,7 @@ if exists(LOG_PATH):
     pass
 else:
     with open(mode='x', file=LOG_PATH) as f:
-        f.write("time, device_name, temperature, humidity\n")
+        f.write("time, device_name, temperature, humidity, dr, milesight_rssi, milesight_snr, dragino_rssi, dragino_snr\n")
 
 
 class SensorData(BaseModel):
@@ -33,20 +33,38 @@ app = FastAPI()
 
 @app.post("/uplink")
 async def create_sensor_data_obj(data: SensorData):
-    time_short = datetime.fromisoformat(data.time).strftime("%Y-%m-%d %H:%M:%S")
+    # account for data sent as UTC (+00:00)
+    time_short = datetime.fromisoformat(data.time) + timedelta(hours=1)
+    time_short = time_short.strftime("%Y-%m-%d %H:%M:%S")
+    
     device_name = data.deviceInfo["deviceName"]
     temperature = data.object["temperature"]
     humidity = data.object.get("humidity")
 
-    # print("Time       :", time_short)
-    # print("Device name:", data.deviceInfo["deviceName"])
-    # print("Temperature:", data.object["temperature"])
-    # print(f"Humidity    {data.object.get('humidity')}%")
-    
-    write_data(time_short, device_name, temperature, humidity)
+    milesight_gw = os.getenv("MILESIGHT_GW")
+    dragino_gw = os.getenv("DRAGINO_GW")
+
+    milesight_rssi = None
+    milesight_snr = None
+    dragino_rssi = None
+    dragino_snr = None
+
+    for i in data.rxInfo:
+        if i["gatewayId"] == milesight_gw:
+            milesight_rssi = i["rssi"]
+            milesight_snr = i["snr"]
+
+        if i["gatewayId"] == dragino_gw:
+            dragino_rssi = i["rssi"]
+            dragino_snr = i["snr"]
+
+    if temperature is None and humidity is None:
+        return data
+
+    write_data(time_short, device_name, temperature, humidity, data.dr, milesight_rssi, milesight_snr, dragino_rssi, dragino_snr)
     return data
 
 
-def write_data(time, device_name, temperature, humidity):
+def write_data(time, device_name, temperature, humidity, dr, milesight_rssi, milesight_snr, dragino_rssi, dragino_snr):
     with open(mode="a", file=LOG_PATH) as f:
-        f.write(f"{time}, {device_name}, {temperature}, {humidity}\n")
+        f.write(f"{time}, {device_name}, {temperature}, {humidity}, {dr}, {milesight_rssi}, {milesight_snr}, {dragino_rssi}, {dragino_snr}\n")
